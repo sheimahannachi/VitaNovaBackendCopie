3cedb476145056e1c3c53a3858841e418259fbe0
yoserzayani <114781649+yoserzayani@users.noreply.github.com>
2024-04-04 00:11:48 +0100

    Update SecurityConfig.java

--- src/main/java/com/example/vitanovabackend/Security/config/SecurityConfig.java
+++ src/main/java/com/example/vitanovabackend/Security/config/SecurityConfig.java
@@ -50,7 +50,17 @@ public class SecurityConfig implements WebSecurityConfigurer {
                 .and()
                 .authorizeHttpRequests()
 
-                .requestMatchers("/api/login","/api/generateToken","/api/getuserfromtoken","/api/sendEmail","/api/reset-password","RestController/GetActiveExercise","RestController/GetExercise",
+                .requestMatchers("/api/login","/api/generateToken","/api/getuserfromtoken","/api/sendEmail","/api/reset-password",
+                        //yoser
+                        "RestController/GetActiveExercise",
+                        "RestController/GetExercise",
+                        "RestController/Rating/{exerciseId}",
+                        "RestController/saveUserExerciseRating/{idEx}",
+                        "RestController/ArchiverExercise/{idex}",
+                        "RestController/UpdateExercise/{id}",
+                        "RestController/addExercise",
+                        "RestController/searchEx",
+
                        //sheima
                         "/PeriodTracker/AddPeriodInformation",
                         "/PeriodTracker/UpdatePeriodinformation",
