b3ad60bc61ba8e2c3a9b9174bd646c5065f07b62
FirasHanini <Firas.Hanini@esprit.tn>
2024-04-20 23:50:35 +0100

    merged user && encrypting messages

--- src/main/java/com/example/vitanovabackend/Service/CommunicationService.java
+++ src/main/java/com/example/vitanovabackend/Service/CommunicationService.java
@@ -1,5 +1,6 @@
 package com.example.vitanovabackend.Service;
 
+import com.example.vitanovabackend.Configuration.EncryptionUtils;
 import com.example.vitanovabackend.DAO.Entities.Communication;
 import com.example.vitanovabackend.DAO.Entities.Community;
 import com.example.vitanovabackend.DAO.Repositories.CommunicationRepository;
@@ -11,9 +12,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Comparator;
+
 import java.util.List;
 
 @Service
@@ -27,26 +29,43 @@ public class CommunicationService implements  ICommunicationService{
     public Communication addCommunication(Communication communication) {
         Communication communicationRecieved;
 
-        if(communication.getCommunity()!=null){
-             communicationRecieved = Communication.builder()
-                    .message(communication.getMessage())
-                    .seen(false)
-                    .sentDate(LocalDate.now())
-                    .sender(communication.getSender())
-                    .community(communication.getCommunity())
-                    .build();
-            return repository.save(communicationRecieved) ;
+        try {
+            String encryptedMessage = EncryptionUtils.encrypt(communication.getMessage());
+
+
+            if (communication.getCommunity() != null) {
+                communicationRecieved = Communication.builder()
+                        .message(encryptedMessage)
+                        .seen(false)
+                        .sentDate(LocalDate.now())
+                        .sender(communication.getSender())
+                        .community(communication.getCommunity())
+                        .build();
+                communicationRecieved=repository.save(communicationRecieved);
+                communicationRecieved.setMessage(
+                        EncryptionUtils.decrypt(communicationRecieved.getMessage())
+                );
+                return communicationRecieved;
+            }
+            if (communication.getReciever() != null) {
+                communicationRecieved = Communication.builder()
+                        .message(encryptedMessage)
+                        .seen(false)
+                        .sentDate(LocalDate.now())
+                        .sender(communication.getSender())
+                        .reciever(communication.getReciever())
+                        .build();
+                communicationRecieved=repository.save(communicationRecieved);
+                communicationRecieved.setMessage(
+                        EncryptionUtils.decrypt(communicationRecieved.getMessage())
+                );
+                return communicationRecieved;
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
         }
-        if(communication.getReciever()!=null) {
-             communicationRecieved = Communication.builder()
-                    .message(communication.getMessage())
-                    .seen(false)
-                    .sentDate(LocalDate.now())
-                    .sender(communication.getSender())
-                    .reciever(communication.getReciever())
-                    .build();
-            return repository.save(communicationRecieved) ;
-        }else return null;
+
+        return null;
 
 
 
@@ -57,6 +76,7 @@ public class CommunicationService implements  ICommunicationService{
         Communication communication1 = repository.findById(id).orElse(null);
         if(communication1==null)
             return null;
+
         communication.setId(id);
         communication.setSeen(false);
 
@@ -103,8 +123,17 @@ public class CommunicationService implements  ICommunicationService{
 
 
         Pageable pageable = PageRequest.of(page, size);
-        return repository.findAllByCommunityOrderByIdDesc(community,pageable);
+         Page<Communication> p =repository.findAllByCommunityOrderByIdDesc(community,pageable);
+         try {
+             for (Communication communication : p) {
+                 String decryptedMessage = EncryptionUtils.decrypt(communication.getMessage());
+                 communication.setMessage(decryptedMessage);
+             }
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
 
+         return p;
     }
 
     @Override
@@ -136,6 +165,8 @@ public class CommunicationService implements  ICommunicationService{
         }
         return true;
     }
+
+
 
 
 }
