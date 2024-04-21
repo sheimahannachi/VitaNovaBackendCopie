f5ebdfb1c5f4fb133c6621982dc33bb9c6b0cf22
sheimahannachi <chaima.hannachi@esprit.tn>
2024-04-21 08:44:13 +0100

    update

--- src/main/java/com/example/vitanovabackend/DAO/Entities/User.java
+++ src/main/java/com/example/vitanovabackend/DAO/Entities/User.java
@@ -3,7 +3,6 @@ package com.example.vitanovabackend.DAO.Entities;
 import com.example.vitanovabackend.Security.config.GrantedAuthorityDeserializer;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import jakarta.persistence.*;
 import lombok.*;
@@ -70,7 +69,6 @@ public class User implements UserDetails {
     PeriodTracker periodTracker;
 
     @OneToOne(cascade = CascadeType.ALL)
-    @JsonManagedReference
     Cart cart;
 
     @ManyToMany(fetch = FetchType.EAGER)
